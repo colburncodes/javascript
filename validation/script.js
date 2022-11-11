c1381d86bfa8c284e2a88dca0fc69859fd53efb2
colburncodes <colburnsanders@gmail.com>
2022-11-11 09:37:55 -0600

    generic validation handlers

--- validation/script.js
+++ validation/script.js
@@ -1,31 +1,42 @@
-const formElement = document.querySelector(".form");
-const formInput = formElement.querySelector("form__input");
-const formError = formElement.querySelector(".form__input-error");
-
-formElement.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-});
-
-const showError = (input, errorMessage) => {
-  input.classList.add("form__input_type_error");
-  formError.textContent = errorMessage;
-  formError.classList.add(".form__input-error-active");
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
 };
 
-const hideError = (input) => {
-  input.classList.remove("form__input_type_error");
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
 };
 
-const checkInputValidity = () => {
-  if (!formInput.validity.valid) {
-    // show error
-    showError(formInput, formInput.validationMessage);
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    // hide error
-    hideError(formInput);
+    hideInputError(formElement, inputElement);
   }
 };
 
-formElement.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-});
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
