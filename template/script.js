a9e9ba36e0c506573064d296526ce11b2eb2a363
colburncodes <colburnsanders@gmail.com>
2022-11-25 12:52:23 -0600

    extended classes inheritance

--- template/script.js
+++ template/script.js
@@ -22,8 +22,80 @@ const items = [
   },
 ];
 
+const cardList = document.querySelector(".card-list__items");
+const popupElement = document.querySelector(".popup");
+const popupImage = document.querySelector(".popup__image");
+const popupCloseButton = document.querySelector(".popup__close");
+const defaultCardButton = document.querySelector(".filter__button_type_grid");
+const horizontalCardButton = document.querySelector(
+  ".filter__button_type_column"
+);
+
 class Card {
+  constructor(cardSelector) {
+    this._cardSelector = cardSelector;
+  }
+
+  _getCardTemplate() {
+    const template = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    return template;
+  }
+
+  _handleMessageClick() {
+    this._element
+      .querySelector(".card__title")
+      .classList.toggle(".card__text_is-active");
+  }
+
+  _handleOpenPopup() {
+    popupImage.src = this._image;
+    popupElement.classList.add("popup_is-opened");
+  }
+
+  _handleClosePopup() {
+    popupImage.src = "";
+    popupElement.classList.remove("popup_is-opened");
+  }
+
+  _setEventListeners() {
+    this._element.addEventListener("click", () => {
+      this._handleOpenPopup();
+    });
+
+    popupCloseButton.addEventListener("click", () => {
+      this._handleClosePopup();
+    });
+  }
+}
+
+class DefaultCard extends Card {
   constructor(data, cardSelector) {
+    super(cardSelector);
+    this._title = data.title;
+    this._image = data.image;
+    this._cardSelector = cardSelector;
+  }
+
+  generateCard() {
+    this._element = super._getCardTemplate();
+    super._setEventListeners();
+
+    this._element.querySelector(
+      ".card__image"
+    ).style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector(".card__title").textContent = this._title;
+
+    return this._element;
+  }
+}
+
+class HorizontalCard extends Card {
+  constructor(data, cardSelector) {
+    super(cardSelector);
     this._title = data.title;
     this._description = data.description;
     this._price = data.price;
@@ -31,16 +103,10 @@ class Card {
     this._cardSelector = cardSelector;
   }
 
-  _getCardTemplate() {
-    const template = document
-      .querySelector(this._cardSelector)
-      .content.cloneNode(true);
-
-    return template;
-  }
-
   generateCard() {
-    this._element = this._getCardTemplate();
+    this._element = super._getCardTemplate();
+    super._setEventListeners();
+
     this._element.querySelector(
       ".card__image"
     ).style.backgroundImage = `url(${this._image})`;
@@ -53,8 +119,24 @@ class Card {
   }
 }
 
-items.forEach((item) => {
-  const card = new Card(item, ".horizontal-card");
-  const cardElement = card.generateCard();
-  document.querySelector(".card-list__items").append(cardElement);
-});
+renderElements = (isGrid) => {
+  cardList.innerHTML = ""; // empty the contents of the container
+
+  items.forEach((item) => {
+    const card = isGrid
+      ? new DefaultCard(item, ".default-card")
+      : new HorizontalCard(item, ".horizontal-card");
+    const cardElement = card.generateCard();
+    document.querySelector(".card-list__items").append(cardElement);
+  });
+
+  defaultCardButton.addEventListener("click", () => {
+    renderElements(true);
+  });
+
+  horizontalCardButton.addEventListener("click", () => {
+    renderElements(false);
+  });
+};
+
+renderElements();
