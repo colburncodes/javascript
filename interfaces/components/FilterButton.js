d24149cc8d418f6a047a4184d49676a7b7cd908f
colburncodes <colburnsanders@gmail.com>
2022-12-01 17:47:45 -0600

    rendering cards

--- interfaces/components/FilterButton.js
+++ interfaces/components/FilterButton.js
@@ -2,10 +2,18 @@ export default class FilterButton {
   constructor({ data, handleButtonClick }, buttonSelector) {
     this._additionalButtonClass = data.buttonClass;
     this._buttonSelector = buttonSelector;
+    this._handleButtonClick = handleButtonClick;
+    this._isGrid = data.isGrid;
+  }
+
+  _setEventListeners() {
+    this._element.addEventListeners(
+      "click",
+      this._handleButtonClick(this._isGrid)
+    );
   }
 
   _getTemplate() {
-    console.log(this._buttonSelector);
     const buttonElement = document
       .querySelector(this._buttonSelector)
       .content.querySelector(".filter__button")
@@ -16,7 +24,10 @@ export default class FilterButton {
 
   generateButton() {
     this._element = this._getTemplate();
+    this._setEventListeners();
+
     this._element.classList.add(this._additionalButtonClass);
+
     return this._element;
   }
 }
--- interfaces/pages/index.js
+++ interfaces/pages/index.js
@@ -1,35 +1,17 @@
+import Section from "../components/Section.js";
+import DefaultCard from "../components/DefaultCard.js";
+import HorizontalCard from "../components/HorizontalCard.js";
+import FilterButton from "../components/FilterButton.js";
 import {
-  items,
   defaultCardButton,
   horizontalCardButton,
   cardListSelector,
+  items,
   filterButtons,
   filterListSelector,
   filterButtonTemplate,
 } from "../utils/constants.js";
-import Section from "../components/Section.js";
-import DefaultCard from "../components/DefaultCard.js";
-import FilterButton from "../components/FilterButton.js";
-import HorizontalCard from "../components/HorizontalCard.js";
 
-const filterList = new Section(
-  {
-    data: filterButtons,
-    renderer: (item) => {
-      const filterButton = new FilterButton(
-        { data: item },
-        filterButtonTemplate
-      );
-
-      const filterButtonElement = filterButton.generateButton();
-
-      filterList.setItem(filterButtonElement);
-    },
-  },
-  filterListSelector
-);
-
-filterList.renderItems();
 const defaultCardList = new Section(
   {
     data: items,
@@ -41,6 +23,7 @@ const defaultCardList = new Section(
   },
   cardListSelector
 );
+
 const horizontalCardList = new Section(
   {
     data: items,
@@ -53,13 +36,32 @@ const horizontalCardList = new Section(
   cardListSelector
 );
 
-defaultCardButton.addEventListener("click", () => {
-  defaultCardList.renderItems(true);
-});
+const filterList = new Section(
+  {
+    data: filterButtons,
+    renderer: (item) => {
+      const filterButton = new FilterButton(
+        {
+          data: item,
+          handleButtonClick: (isGrid) => {
+            if (isGrid) {
+              defaultCardList.clear();
+              defaultCardList.renderItems();
+            } else {
+              horizontalCardList.clear();
+              horizontalCardList.renderItems();
+            }
+          },
+        },
+        filterButtonTemplate
+      );
 
-horizontalCardButton.addEventListener("click", () => {
-  horizontalCardList.renderItems(false);
-});
+      const filterButtonElement = filterButton.generateButton();
+      filterList.setItem(filterButtonElement);
+    },
+  },
+  filterListSelector
+);
 
-filterButtons;
-
+defaultCardList.renderItems();
+filterList.renderItems();
